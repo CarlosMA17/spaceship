1e11307c4af65272248d37754bd88eedbeeec13c
Carlos Martel <carlosma0117@gmail.com>
2022-04-21 13:12:13 +0100

    initial commit branch interfaz: commit inicial para crear la interfaz

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int respuesta = sc.nextInt();
         if (respuesta == 1) {
-
             int peso = nave.load();
             if (peso == 11){
                 String mensaje = "el cargo "+ nave.nombre +" esta completo, no se puede añadir mas";
                 peso = 10;
                 System.out.println(mensaje);
-
             }
             nave = new Cargo("nave", "1235bdj", 1, 1, 0, peso);
         }
+
+        System.out.println("desea retirar un container del cargo?");
+        sc = new Scanner(System.in);
+        respuesta = sc.nextInt();
+        if (respuesta == 1) {
+            int peso = nave.unload();
+            if (peso == 12){
+                String mensaje = "el cargo "+ nave.nombre +" esta vacio, no se puede retirar mas";
+                peso = 0;
+                System.out.println(mensaje);
+            }
+            nave = new Cargo("nave", "1235bdj", 1, 1, 0, peso);
+        }
+
+        System.out.println("nombre: " + nave.nombre + "\n" +
+                "matricula " + nave.matricula + "\naceleracion " + nave.aceleracion + "\nvelocidad " + nave.velocidadX +
+                "\npeso: " + nave.cargo + "\n\n");
+
+        System.out.println(nave.toString());
 
 
     }
